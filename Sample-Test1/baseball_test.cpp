f79fc0fa0cc644e98e2aa4acc9e9e1b23540f922
kcha2299 <kcha2299@gmail.com>
2024-05-17 14:01:10 +0900

    [feature] page 27

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,10 +1,8 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "../Project8-4/baseball.cpp"
 
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(10, 10);
-  EXPECT_EQ(10, 10);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
